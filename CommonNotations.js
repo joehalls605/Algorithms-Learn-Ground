c01415c46508d233b1b551d10b5245440b5847ee
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-01-04 11:27:38 +0000

    CommonNotations

---
+++ CommonNotations.js
@@ -0,0 +1,32 @@
+// COMMON NOTATIONS
+
+/*
+
+O-Notation (Big O):
+
+O(f(n))
+Explanation: Represents the upper bound or worst-case time complexity of an algorithm. It provides an asymptotic upper limit on the growth rate of the algorithm's running time concerning the input size.
+Example: If an algorithm is O(n^2), it means the running time grows quadratically with the input size.
+Ω-Notation (Big Omega):
+
+Ω(f(n))
+Explanation: Represents the lower bound or best-case time complexity of an algorithm. It provides an asymptotic lower limit on the growth rate of the algorithm's running time concerning the input size.
+Example: If an algorithm is Ω(n), it means the running time grows at least linearly with the input size.
+Θ-Notation (Theta):
+
+Θ(f(n))
+Explanation: Represents the tight or average-case time complexity of an algorithm. It provides both upper and lower bounds, indicating that the growth rate is neither faster nor slower than the specified function.
+Example: If an algorithm is Θ(n), it means the running time grows linearly with the input size, and it is both the best and worst-case scenario.
+o-Notation (Small O):
+
+o(f(n))
+Explanation: Represents an upper bound that is not tight. It signifies that the growth rate of the algorithm's running time is strictly less than the specified function.
+Example: If an algorithm is o(n^2), it means the running time grows strictly slower than quadratic with the input size.
+ω-Notation (Small Omega):
+
+ω(f(n))
+Explanation: Represents a lower bound that is not tight. It signifies that the growth rate of the algorithm's running time is strictly greater than the specified function.
+Example: If an algorithm is ω(n), it means the running time grows strictly faster than linear with the input size.
+
+
+*/
