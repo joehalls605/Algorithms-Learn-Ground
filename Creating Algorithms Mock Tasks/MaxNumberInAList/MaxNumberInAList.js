8593d6f6fe4e4db7ee777d713950684e69ecd449
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-04-24 09:23:41 +0100

    MaxNumberInAList

---
+++ Creating Algorithms Mock Tasks/MaxNumberInAList/MaxNumberInAList.js
@@ -0,0 +1,32 @@
+// Algorithm: Finding the Maximum Number in a List
+
+/*
+
+Algorithm:
+
+1. Start with the assumption that the first number in the list is the maximum
+2. Iterate through the list, starting from the second number
+3. For each number in thre list:
+    If the number is greater than the assumed maximum, update the maximum to be the current number
+    If the current number is not greater than the assumed maximum, move to the next number
+4. After iterating throught he entire list, the assumed maximum will be the actual maximum number in the list
+5. Return the maximum number found
+*/
+
+// Pseudocode:
+
+function findMax(list){
+    // Assume the first number in the list is the maximum
+    let maxNumber = list[0];
+
+    for(let i = 1; i < list.length; i++){
+        const number = list[i];
+        if(number > maxNumber){
+            maxNumber = number;
+        }
+    }
+
+    return maxNumber;
+
+
+}
