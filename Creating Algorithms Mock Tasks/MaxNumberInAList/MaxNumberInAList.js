5b01acdcb14dd653fafeda997c33e8f6f5bcf824
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-04-24 09:28:59 +0100

    definition of why array data structure used

--- Creating Algorithms Mock Tasks/MaxNumberInAList/MaxNumberInAList.js
+++ Creating Algorithms Mock Tasks/MaxNumberInAList/MaxNumberInAList.js
@@ -11,9 +11,36 @@ Algorithm:
     If the current number is not greater than the assumed maximum, move to the next number
 4. After iterating throught he entire list, the assumed maximum will be the actual maximum number in the list
 5. Return the maximum number found
+
 */
 
-// Pseudocode:
+/* Pseudocode:
+
+function findMax(list):
+    // Assume the first number in the list is the maximum
+    maxNumber = list[0]
+    
+    // Iterate through the list starting from the second number
+    for number in list[1:]:
+        // Update maxNumber if the current number is greater
+        if number > maxNumber:
+            maxNumber = number
+    
+    // Return the maximum number found
+    return maxNumber
+*/
+
+/*
+Data structure used: Array
+
+Why:
+This is because the algorithm needs to access each number in the list sequentially to compare them.
+You need individual access to the elements by index
+The ordered collection is good
+*/
+
+
+
 
 function findMax(list){
     // Assume the first number in the list is the maximum
